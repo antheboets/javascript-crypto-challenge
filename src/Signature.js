b366fbfc242d7152f6623dd522f00430f4432ab6
antheboets <anthe.boets@gmail.com>
2019-10-05 20:06:43 +0200

    adding body to Signature module functions

--- src/Signature.js
+++ src/Signature.js
@@ -1,6 +1,14 @@
-exports.verifyingKey = async function(){
+const nacl = require('libsodium-wrappers');
+let myKeypairObj;
 
+beforeAll(async() => {
+    await nacl.ready;
+    myKeypairObj = nacl.crypto_sign_keypair();
+});
+
+exports.verifyingKey = async function(){
+    return myKeypairObj.publicKey;
 }
 exports.sign = async function(msg){
-
+    return nacl.crypto_sign(msg, myKeypairObj.privateKey);
 }
