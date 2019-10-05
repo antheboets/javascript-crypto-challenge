21503dac28187ce7293e5236a425a0dce38412cd
antheboets <anthe.boets@gmail.com>
2019-10-05 18:06:24 +0200

    adding body to Decryptor module functions

--- src/Decryptor.js
+++ src/Decryptor.js
@@ -1,4 +1,11 @@
+const nacl = require('libsodium-wrappers');
+let myKey;
 exports.decrypt = async function (ciphertext, nonce){
+    if(myKey == null){
+        throw "no key";
+    }
+    return  nacl.crypto_secretbox_open_easy(ciphertext,nonce,myKey);
 }
 exports.setKey = async function (key){
+    myKey = key;
 }
